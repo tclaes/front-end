92d8cbf69ba9a7649fc63b3107688260e9e5b4b6
Tom Claes <tom.claes@cegeka.com>
2020-09-26 22:01:01 +0200

    converted to nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Front End <span style="color: red">.</span></h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+    body {
+      background: #333;
+      display: grid;
+        place-content: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+      width: 100vw;
+    }
+  h1 {
+    color: rgb(255, 255, 255);
+    font-family: 'Fredoka One', cursive;
+    font-size: 78px;
+    padding: 2rem;
+  }
+</style>

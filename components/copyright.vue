be6c9d24ebcec529795af27e3cd8fac553fbb73b
Tom Claes <tom.claes@cegeka.com>
2020-09-26 23:55:56 +0200

    copyright added

---
+++ components/copyright.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>&copy; {{currentDate.getFullYear()}} - {{ siteName.toUpperCase() }} </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+     return {
+        currentDate: new Date,
+        siteName: 'front-end.eu',
+      }
+ }
+  }
+</script>
+
+<style lang="scss" scoped>
+  p{
+    color: #fff;
+    font-family: 'Fredoka One', sans-serif;
+    font-size: .8rem;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,34 @@
 <template>
+<div class="grid">
     <h1 class='title'>Front End</h1>
+    <Copyright class="copyright" />
+</div>
 </template>
 
 <script>
+  import Copyright from '~/components/copyright';
   export default {
-    
+    components: {
+      Copyright
+    }
   }
 </script>
 
 <style scoped>
+  .grid {
+    display: grid;
+      grid-template-rows: 1fr 150px;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+  }
+
   h1 {
     color: rgb(255, 255, 255);
+    display: flex;
+    place-content: center;
+    align-items: center;
     font-family: 'Fredoka One', cursive;
     font-size: 78px;
     padding: 1rem;
@@ -22,4 +40,13 @@
     display: inline-block;
     padding-left: 1rem;
   }
+
+  .copyright {
+    display: flex;
+    grid-row-start: 2;
+    justify-content: flex-end;
+    align-items: end;
+    padding: 0 1rem;
+  }
+
 </style>

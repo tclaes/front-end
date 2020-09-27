f11b25963821e9230d8c985ec1d67b8211b02073
Tom Claes <tom.claes@cegeka.com>
2020-09-27 22:36:27 +0200

    Nuxt page added & created navigation component

--- .nuxt/mixins/fetch.server.js
+++ .nuxt/mixins/fetch.server.js
@@ -1,55 +1,53 @@
-import Vue from "vue";
-import { hasFetch, normalizeError, addLifecycleHook } from "../utils";
+import Vue from 'vue'
+import { hasFetch, normalizeError, addLifecycleHook } from '../utils'
 
 async function serverPrefetch() {
   if (!this._fetchOnServer) {
-    return;
+    return
   }
 
   // Call and await on $fetch
   try {
-    await this.$options.fetch.call(this);
+    await this.$options.fetch.call(this)
   } catch (err) {
     if (process.dev) {
-      console.error("Error in fetch():", err);
+      console.error('Error in fetch():', err)
     }
-    this.$fetchState.error = normalizeError(err);
+    this.$fetchState.error = normalizeError(err)
   }
-  this.$fetchState.pending = false;
+  this.$fetchState.pending = false
 
   // Define an ssrKey for hydration
-  this._fetchKey = this.$ssrContext.nuxt.fetch.length;
+  this._fetchKey = this.$ssrContext.nuxt.fetch.length
 
   // Add data-fetch-key on parent element of Component
-  const attrs = (this.$vnode.data.attrs = this.$vnode.data.attrs || {});
-  attrs["data-fetch-key"] = this._fetchKey;
+  const attrs = this.$vnode.data.attrs = this.$vnode.data.attrs || {}
+  attrs['data-fetch-key'] = this._fetchKey
 
   // Add to ssrContext for window.__NUXT__.fetch
-  this.$ssrContext.nuxt.fetch.push(
-    this.$fetchState.error ? { _error: this.$fetchState.error } : this._data
-  );
+  this.$ssrContext.nuxt.fetch.push(this.$fetchState.error ? { _error: this.$fetchState.error } : this._data)
 }
 
 export default {
   created() {
     if (!hasFetch(this)) {
-      return;
+      return
     }
 
-    if (typeof this.$options.fetchOnServer === "function") {
-      this._fetchOnServer = this.$options.fetchOnServer.call(this) !== false;
+    if (typeof this.$options.fetchOnServer === 'function') {
+      this._fetchOnServer = this.$options.fetchOnServer.call(this) !== false
     } else {
-      this._fetchOnServer = this.$options.fetchOnServer !== false;
+      this._fetchOnServer = this.$options.fetchOnServer !== false
     }
 
     // Added for remove vue undefined warning while ssr
-    this.$fetch = () => {}; // issue #8043
-    Vue.util.defineReactive(this, "$fetchState", {
+    this.$fetch = () => {} // issue #8043
+    Vue.util.defineReactive(this, '$fetchState', {
       pending: true,
       error: null,
-      timestamp: Date.now(),
-    });
+      timestamp: Date.now()
+    })
 
-    addLifecycleHook(this, "serverPrefetch", serverPrefetch);
-  },
-};
+    addLifecycleHook(this, 'serverPrefetch', serverPrefetch)
+  }
+}
---
+++ pages/Nuxt.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <h1>Nuxt playground</h1>
+  </Layout>
+</template>
+
+<script>
+  import Copyright from "~/components/copyright";
+  import Layout from "~/components/Layout";
+  export default {
+    components: {
+      Copyright,
+      Layout,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
